fba1c579c78705e3a8aa96982fc4e8f7eb51652d
ankitkushawaha1000 <ankitkushawaha1000@gmail.com>
2021-10-31 09:24:36 +0530

    improved navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,45 +2,45 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavLink } from "react-router-dom";
 import logo from "../images/KhataBook.jpg"
+import Nav from 'react-bootstrap/Nav'
 
 const Navbar = () => {
     return (
         <>
-        
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Nav variant="pills" className="navbar navbar-expand-lg navbar-dark bg-dark">
             
-  <NavLink className="navbar-brand" to="#" >
-        <img src={logo} alt="logo" />
-  </NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+          <NavLink to="#" >
+            <img src={logo} alt="logo" />
+          </NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ms-auto">
-    <li className="nav-item">
-        <NavLink className="nav-link" to="/">Home</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/DailyTransaction">Daily Transaction</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/DashBoard">DashBoard</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/PlanBudget">Plan Budget</NavLink>
-      </li>
-     
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/Login">Login</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/Signup">Registration</NavLink>
-      </li>
-    </ul>
-   
-  </div>
-</nav>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                  <NavLink className="nav-link" to="/Home">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/DailyTransaction">Daily Transaction</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/DashBoard">DashBoard</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/PlanBudget">Plan Budget</NavLink>
+              </li>
+            
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/Login">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/Signup">Registration</NavLink>
+              </li>
+            </ul>
+          
+          </div>
+        </Nav>
 
         </>
     )
